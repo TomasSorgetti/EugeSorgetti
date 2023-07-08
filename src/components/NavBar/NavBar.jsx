47fe3ef4c6994c0a976b4f2576be59de367c91ed
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2023-07-08 15:02:48 -0300

    navbar mobile fixed and onClose

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,9 +8,12 @@ const NavBar = () => {
   const handleClick = () => {
     setNav(!nav)
   }
+  const closeNav = () => {
+    setNav(false)
+  }
   return (
-    <nav className="flex items-center justify-between flex-wrap p-6">
-      <h3>euge sorgetti</h3>
+    <nav className="flex items-center justify-between flex-wrap p-6 fixed bg-background2 w-full">
+      <h3 className="font-semibold">euge sorgetti</h3>
       <div className="block lg:hidden">
         <button
           onClick={() => handleClick()}
@@ -32,28 +35,39 @@ const NavBar = () => {
         }`}
       >
         <div className="text-sm lg:flex-grow text-center">
-          <a href="#about" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
+          <a
+            onClick={closeNav}
+            href="#about"
+            className="block mt-4 lg:inline-block lg:mt-0 mr-4"
+          >
             about
           </a>
           <a
+            onClick={closeNav}
             href="#portfolio"
             className="block mt-4 lg:inline-block lg:mt-0 mr-4"
           >
             portfolio
           </a>
-          <a href="#getintouch" className="block mt-4 lg:inline-block lg:mt-0">
+          <a
+            onClick={closeNav}
+            href="#getintouch"
+            className="block mt-4 lg:inline-block lg:mt-0"
+          >
             get in touch
           </a>
         </div>
         <div className="flex justify-center">
           <a
-            href="http://behance.net/eugesorgetti" target="blank"
+            href="http://behance.net/eugesorgetti"
+            target="blank"
             className="inline-block text-sm px-4 py-2 leading-none border rounded mt-4 lg:mt-0"
           >
             <BsBehance size={"1.5rem"} />
           </a>
           <a
-            href="http://www.linkedin.com/in/eugenia-sorgetti" target="blank"
+            href="http://www.linkedin.com/in/eugenia-sorgetti"
+            target="blank"
             className="inline-block text-sm px-4 py-2 leading-none border rounde mt-4 lg:mt-0"
           >
             <BiLogoLinkedin size={"1.5rem"} />
